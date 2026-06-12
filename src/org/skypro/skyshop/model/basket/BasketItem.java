8b28a8629bf7c681ee782db4a7f74d50e7434df8
Elehub19 <lopasheva@me.com>
2026-06-13 00:40:20 +0500

    send1

--- src/org/skypro/skyshop/model/basket/BasketItem.java
+++ src/org/skypro/skyshop/model/basket/BasketItem.java
@@ -1,7 +1,5 @@
 package org.skypro.skyshop.model.basket;
 
-import org.skypro.skyshop.model.product.Product;
-
 public class BasketItem {
     private final Product product;
     private final int quantity;
