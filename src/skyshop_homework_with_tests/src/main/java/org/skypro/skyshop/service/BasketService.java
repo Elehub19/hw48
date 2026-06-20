b4f51311b83a9a70aa7b8da46c809f3153a397a8
Elehub19 <lopasheva@me.com>
2026-06-20 19:04:44 +0500

    send6

---
+++ src/skyshop_homework_with_tests/src/main/java/org/skypro/skyshop/service/BasketService.java
@@ -0,0 +1,39 @@
+package org.skypro.skyshop.service;
+
+import org.skypro.skyshop.model.basket.BasketItem;
+import org.skypro.skyshop.model.basket.ProductBasket;
+import org.skypro.skyshop.model.basket.UserBasket;
+import org.skypro.skyshop.exception.NoSuchProductException;
+import org.skypro.skyshop.model.product.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class BasketService {
+    private final ProductBasket productBasket;
+    private final StorageService storageService;
+
+    @Autowired
+    public BasketService(ProductBasket productBasket, StorageService storageService) {
+        this.productBasket = productBasket;
+        this.storageService = storageService;
+    }
+
+    public void addProduct(UUID id) {
+        Product product = storageService.getProductById(id)
+                .orElseThrow(() -> new NoSuchProductException("Продукт с id " + id + " не найден"));
+        productBasket.addProduct(product.getId());
+    }
+
+    public UserBasket getUserBasket() {
+        return new UserBasket(productBasket.getProducts().entrySet().stream()
+                .map(entry -> {
+                    Product product = storageService.getProductById(entry.getKey())
+                            .orElseThrow(() -> new NoSuchProductException("Продукт с id " + entry.getKey() + " не найден"));
+                    return new BasketItem(product, entry.getValue());
+                })
+                .collect(Collectors.toList()));
+    }
+}
